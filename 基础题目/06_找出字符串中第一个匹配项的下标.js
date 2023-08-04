eb6728e154ee84bb5d3e62c7266e8f31df68b39a
codergb <betweent@126.com>
2023-08-04 22:05:19 +0800

    kmp

--- 基础题目/06_找出字符串中第一个匹配项的下标.js
+++ 基础题目/06_找出字符串中第一个匹配项的下标.js
@@ -1,5 +1,6 @@
-var strStr = function(haystack, needle) {
-
+var strStr = function(haystack, needle) {//kmp算法
+  let n = needle.length, m = haystack.length;
+  
 };
 let haystack = "sadbutsad", needle = "sad";
 console.log(strStr(haystack,needle));
