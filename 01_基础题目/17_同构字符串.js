b4bf8545be8ccdd8426dadec18756f1fda633989
codergb <betweent@126.com>
2023-08-06 15:51:43 +0800

    多数元素

--- 01_基础题目/17_同构字符串.js
+++ 01_基础题目/17_同构字符串.js
@@ -1,1 +1,20 @@
-同构字符串
+var isIsomorphic = function(s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+  let sMap = {};
+  let tMap = {};
+  for (let i = 0; i < s.length; i++){
+    let charS = s.charAt(i);
+    let charT = t.charAt(i);
+    if (sMap[charS] !== charT || tMap[charT]!== charS) {
+      return false;
+    }
+    sMap[charS] = charT;
+    tMap[charT] = charS;
+  }
+ 
+  return true;
+};
+let s = "egg", t = "add";
+isIsomorphic(s,t)
